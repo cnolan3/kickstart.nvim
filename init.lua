dff925fcbaccfcd6dde2adb2394b3b1e40ec045f
Connor Nolan <connornolandev@gmail.com>
2024-08-25 14:29:28 -0700

    make nvim background transparent

--- init.lua
+++ init.lua
@@ -920,11 +920,11 @@ require('lazy').setup({
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('kanagawa').setup {
+        transparent = true,
         styles = {
           comments = { italic = false }, -- Disable italics in comments
         },
       }
-
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
